8e3f2d93f0df534261dd94a78f96509f4eeb51fa
Belal Abujaber <111012121+AbuBatata7@users.noreply.github.com>
2024-03-25 08:25:37 -0500

    h

---
+++ functions.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php
+
+    echo str_replace("Belal","Dylan","Hi Belal");
+
+?>
+
+
+    
+</body>
+</html>
